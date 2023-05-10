cfba0046697aa3d59a19ba4ba8bc44e036dd92f3
hoefkensj <hoefkens.j@gmail.com>
2023-05-10 02:57:22 +0200

    Update MSRFlagEdit.c, ZeroProcHot.c, build.c.sh, and 3 more files

--- ZeroProcHot.c
+++ ZeroProcHot.c
@@ -1,9 +1,3 @@
-//
-// ############################################################################
-// # REPO: github.com/hoefkensj/ZeroProcHot      AUTHOR: Hoefkens.j@gmail.com #
-// # FILE: ZeroProcHot_fast.c                   LICENCE: MIT       2023-05-01 #
-// ############################################################################
-//
 #include <efibind.h>
 #include <efidef.h>
 #include <efidevp.h>
@@ -11,233 +5,253 @@
 #include <efiprot.h>
 #include <efiapi.h>
 #include <efierr.h>
+#include <stdlib.h>
+
+#define MSR_ADDRESS 0x1FC
+#define VERSION "0v402"
+#define HEAD0 L"(c)2023 HoefkensJ            Zero BDProcHot 0v402        github.com/hoefkensj"
+#define HEAD1 L"\r\n-------------------------------------------------------------------------------\r\n"
+#define TBL_HL L"     = ------------------------------------------------------------------"
+
+#define DEF_STALL 100000
+#define MSG_CONFIRM  L"\r\n\r\n\r\nWrite Result to System? (Y/n) "
+#define MSG_INTERACTIVE L"ZeroProcHot : Press Esc for interactive mode "
+#define MSG_ABORT L"\r\nAborted by user!"
+#define MSG_ENABLED L"\r\nBD_PROCHOT is now Enabled!  "
+#define MSG_DISABLED L"\r\nBD_PROCHOT is now Disabled!  "
+#define CLEAR_ZERO 0xFFFFFFFFFFFFFFFE
+#define SET_ZERO 1
 
 EFI_SYSTEM_TABLE *sT;
 EFI_BOOT_SERVICES *tbS;
 SIMPLE_TEXT_OUTPUT_INTERFACE *tcO;
-SIMPLE_INPUT_INTERFACE  *tcI;
+SIMPLE_INPUT_INTERFACE *tcI;
 EFI_TEXT_OUTPUT_STRING strOut;
-EFI_TEXT_RESET strClear;
+EFI_TEXT_RESET scrClear;
 EFI_INPUT_READ_KEY keyRead;
-
+EFI_STALL bsSlp;
 EFI_WAIT_FOR_EVENT evtWait;
-EFI_EVENT keyWait;         
+EFI_EVENT keyWait;
 
 
-static uint64_t AsmReadMsr64(uint32_t index) {
-	uint32_t low;
-	uint32_t high;
-	uint64_t val;
-	__asm__ __volatile__("rdmsr"
-	                     : "=a"(low),
-	                       "=d"(high)
-	                     : "c"(index));
-	val = ((uint64_t)high << 32) | low;
-	return val;
+
+void ConfirmAndWrite(uint64_t val,CHAR16 *msg) ; 
+void choose(uint8_t choice);
+void menu (uint8_t choice);
+
+void strRow(CHAR16 *head,uint64_t data);
+void print_bits(uint64_t val);
+void clear_ProcHot();
+void read_ProcHot();
+void set_ProcHot();
+uint8_t menuloop ();
+uint64_t read_MSR();
+uint8_t toMsg(CHAR16*msg, uint64_t to,CHAR16 *prog);
+uint64_t
+AsmReadMsr64(uint32_t addr) {
+  uint32_t low;
+  uint32_t high;
+  uint64_t val;
+  __asm__ __volatile__("rdmsr"
+                       : "=a"(low),
+                         "=d"(high)
+                       : "c"(addr));
+  val = ((uint64_t)high << 32) | low;
+  return val;
 }
 
-static uint64_t AsmWriteMsr64(uint32_t index, uint64_t val) {
-	uint32_t low;
-	uint32_t high;
-	low = (uint32_t)(val);
-	high = (uint32_t)(val >> 32);
+uint64_t
+AsmWriteMsr64(uint32_t addr, uint64_t val) {
+  uint32_t low;
+  uint32_t high;
+  low = (uint32_t)(val);
+  high = (uint32_t)(val >> 32);
 
-	__asm__ __volatile__("wrmsr"
-	                     :
-	                     : "c"(index),
-	                       "a"(low),
-	                       "d"(high));
-	return val;
-}
-void strHeader();
-
-void print_bit(uint64_t val, int bit) {
-	if (val & ((uint64_t)1 << bit))
-	    strOut(tcO, L"1");
-	else
-	    strOut(tcO, L"0");
-}
-
-void print_bits(uint64_t val) {
-	for (int i = 63; i >= 0; i--) {
-	    strOut(tcO, (val & (1ull << i)) ? L"1" : L"0");
-	}
-}
-void strRow(CHAR16 *head,uint64_t data){
-	strOut(tcO, L"\r\n");
-	strOut(tcO, head);
-	print_bits( data);
-
-}
-int getConfirmation() {
-	int result = 1;
-	uint64_t index;
-	EFI_INPUT_KEY key;
-
-
-	strOut(tcO, L"(Y/n)"); 
-	evtWait(1, keyWait, &index);
-
-	keyRead(sT->ConIn, &key);
-	if (key.UnicodeChar == 'n' || key.UnicodeChar == 'N') {
-			strOut(tcO, L"\r\nAborted by user!\r\n"); 
-	    result = 0;
-	}
-	return result;
-}
-
-
-uint64_t 
-set_ProcHot() {
-	uint64_t val = AsmReadMsr64(0x1FC);
-	uint64_t setbitzero = 1;  
-	uint64_t nval = val | setbitzero;
-	CHAR16 *tblADR  =L"0x1FC:  ";
-	CHAR16 *tblOR   =L"   OR:  ";
-	CHAR16 *tblHL   =L"------------------------------------------------------------------------------";
-	CHAR16 *tblRES  =L"RESULT:  ";
-	strRow(tblADR,val);	
-	strRow(tblOR,setbitzero);
-	strOut(tcO,tblHL);	
-	strRow(tblRES,nval);
-	return nval;
-}
-
-uint64_t 
-clear_ProcHot(){
-
-	uint64_t val = AsmReadMsr64(0x1FC);
-	uint64_t clearbitzero =  0xFFFFFFFFFFFFFFFE;
-	uint64_t nval = val & clearbitzero;    
-	CHAR16 *tblADR  =L"\r\n 0x1FC:  ";
-	CHAR16 *tblAND  =L"\r\n   AND:  ";
-	CHAR16 *tblHL   =L"\r\n------------------------------------------------------------------------------";
-	CHAR16 *tblRES  =L"\r\nRESULT:  ";
-	strOut(tcO, tblADR);
-	strOut(tcO, L"\r\n");
-	strOut(tcO, tblAND);
-	strOut(tcO, L"\r\n");
-	strOut(tcO, tblHL);
-	strOut(tcO, L"\r\n");	
-	strOut(tcO, tblRES);
-	return nval;
-}
-
-int read_ProcHot(){
-	uint64_t val = AsmReadMsr64(0x1FC);
-	uint64_t bit = val & 1;
-	if (bit == 1){
-	    strOut(tcO, L"\r\nBD_PROCHOT: 1"); 
-	}else { 
-	    strOut(tcO, L"\r\nBD_PROCHOT: 0");
-	}
-	return bit;
-}
-
-void menu (int idx){
-	CHAR16 *menu[4];
-
-	menu[0]= L"1. Read BD_PROCHOT";
-	menu[1]= L"2. Clear BD_PROCHOT";
-	menu[2]= L"3. Set BD_PROCHOT";
- 	menu[3]= L"0. Exit";
-
-	for (int i = 0; i < 4; i++)  {
-		if ( i == idx){
-			strOut(tcO, L"> ");
-		} else {
-			strOut(tcO, L"  ");
-		}
-		strOut(tcO, menu[i]);
-		strOut(tcO, L"\r\n");
-	}
-}
-
-void menuloop () {
-	EFI_INPUT_KEY key;
-	uint64_t nval,index;
-
-	
-mnu:	strClear(tcO,FALSE);	
-	strHeader(tcO);
-	strOut(tcO, L"Menu : ");
-	strOut(tcO, L"\r\n-------------\r\n");
-	menu(0);
-
-	evtWait(1, &keyWait, &index);
-	keyRead(tcI, &key);
-
-	strOut(tcO,&key.UnicodeChar);
-
-	while (1) {
-		if (key.UnicodeChar == '0')  {
-			break;
-		}	else if (key.UnicodeChar == '1'){ 
-			read_ProcHot();
-		}	else if (key.UnicodeChar == '2'){
-			nval= clear_ProcHot();
-			if (getConfirmation(sT) == 1) { 
-				AsmWriteMsr64(0x1FC, nval); 
-				strOut(tcO, L"\r\nBD_PROCHOT is now Disabled!\r\n"); 
-			}else{  
-				strOut(tcO, L"\r\nAborted by user!\r\n");
-			}
-			goto mnu;
-	  }
-	else if (key.UnicodeChar == '3'){
-		nval=set_ProcHot();
-		if (getConfirmation() == 1) {
-			AsmWriteMsr64(0x1FC, nval);
-			strOut(tcO, L"\r\nBD PROCHOT is now enabled!\r\n");
-		
-		}
-	}
-	else { goto mnu; }
-}
+  __asm__ __volatile__("wrmsr"
+                       :
+                       : "c"(addr),
+                         "a"(low),
+                         "d"(high));
+  return val;
 }
 
 EFI_STATUS
 efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systemTable) {
-	EFI_SYSTEM_TABLE *sT                = systemTable;
-	EFI_BOOT_SERVICES *tbS              = sT->BootServices;
-	SIMPLE_TEXT_OUTPUT_INTERFACE *tcO   = sT->ConOut;
-	SIMPLE_INPUT_INTERFACE  *tcI        = sT->ConIn;
-
-	EFI_TEXT_OUTPUT_STRING strOut       = tcO->OutputString;
-	EFI_TEXT_RESET strClear             = tcO->Reset;
-	EFI_INPUT_READ_KEY keyRead          = tcI->ReadKeyStroke;
-	EFI_STALL bsSlp											= tbS->Stall;
-	EFI_INPUT_KEY key;
-	CHAR16 *toMsg = L"ZeroProcHot : Press Esc for interactive mode ";
-
-	//clear the screen 
-	strClear(tcO,FALSE);	
-	strOut(tcO,toMsg);
-
-	for(int i=0;i<10;i++) {
-		// sT->BootServices->WaitForEvent(1, &sT->ConIn->WaitForKey, &index);
-		// print a dot to the screen
-	  strOut(tcO, L".");
-	  keyRead(tcI, &key);
-	  if (key.ScanCode == SCAN_ESC || key.UnicodeChar == '`') {
-			strClear(tcO,FALSE);	
-			strHeader();
-			menuloop();
-		} else {            
-	  	bsSlp(100000);
-	  }
-	}
-  
-	return EFI_SUCCESS;
+  sT = systemTable;
+  tbS = sT->BootServices;
+  tcO = sT->ConOut;
+  tcI = sT->ConIn;
+  strOut = tcO->OutputString;
+  scrClear = tcO->Reset;
+  keyRead = tcI->ReadKeyStroke;
+  bsSlp = tbS->Stall;
+  CHAR16 *msg = MSG_INTERACTIVE;
+  uint8_t choice = 1 ;
+  //clear the screen 
+  scrClear(tcO,FALSE);  
+  if (toMsg(msg,10,L".")){
+    while  (choice != 0){ 
+      choice=menuloop();
+      choose(choice);
+    }
+  } 
+  return EFI_SUCCESS;
 }
-void strHeader() {
-  CHAR16 *head[5];
-  head[0] = L"\r\n###############################################################################";
-  head[1] = L"\r\n##                              Zero BDProcHot                               ##";
-  head[2] = L"\r\n##---------------------------------------------------------------------------##";
-  head[3] = L"\r\n## (c)2023 HoefkensJ                        https://www.github.com/hoefkensj ##";
-  head[4] = L"\r\n###############################################################################\r\n\r\n";
-  for (int i = 0; i < 5; i++) { 
-    strOut(tcO, head[i]);
+
+uint8_t
+toMsg(CHAR16*msg, uint64_t to,CHAR16 *prog){
+  uint8_t keypressed = 0;
+  EFI_INPUT_KEY key;
+
+  strOut(tcO,msg);
+  for(int i=0;i<to;i++) {
+    if (i%5 == 0) strOut(tcO, prog);     
+    keyRead(tcI, &key);
+    if (key.UnicodeChar == CHAR_CARRIAGE_RETURN|| key.ScanCode == SCAN_ESC || key.UnicodeChar == '`') {
+      keypressed=1;
+      break;
+    }
+    bsSlp(DEF_STALL);
+  }
+  return keypressed;
+}
+
+void
+ConfirmAndWrite(uint64_t val,CHAR16 *msg) {
+
+  uint64_t index;
+  strOut(tcO, MSG_CONFIRM); 
+  sT->BootServices->WaitForEvent(1, &sT->ConIn->WaitForKey, &index);
+  EFI_INPUT_KEY key;
+  sT->ConIn->ReadKeyStroke(sT->ConIn, &key);
+  scrClear(tcO,FALSE);  
+  if ( key.ScanCode    == SCAN_ESC ||
+       key.UnicodeChar == 'n'      ||
+       key.UnicodeChar == 'N') {      
+      toMsg(MSG_ABORT, 20, L""); 
+  } else {
+      AsmWriteMsr64(0x1FC, val); 
+      toMsg(msg, 20, L""); 
   }
 }
+
+void
+menu (uint8_t choice){
+  CHAR16 *menu[4];
+  int idx=((choice+3)%4);
+
+  menu[0]= L"1. Read 0x1FC";
+  menu[1]= L"2. Clear BD_PROCHOT";
+  menu[2]= L"3. Set BD_PROCHOT";
+  menu[3]= L"0. Exit";
+  strOut(tcO, L"\r\n\r\nMenu : ");
+  strOut(tcO, L"\r\n----------------------\r\n");
+  for (int i = 0; i < 4; i++)  {
+    if ( i == idx){
+      strOut(tcO, L"> ");
+    } else {
+      strOut(tcO, L"  ");
+    }
+    strOut(tcO, menu[i]);
+    strOut(tcO, L"\r\n");
+  }
+}
+
+void
+choose(uint8_t choice){
+  uint64_t index;
+  switch (choice) {
+    case 1:
+      read_MSR();
+      sT->BootServices->WaitForEvent(1, &sT->ConIn->WaitForKey, &index);
+      break;
+    case 2:
+      clear_ProcHot();
+      break;
+    case 3:
+      set_ProcHot();
+      break;
+    default: break;
+  }
+}
+
+uint8_t
+menuloop () {
+  uint64_t index;
+  uint8_t choice = 1;
+  CHAR16 keychar;
+  EFI_INPUT_KEY key;
+  while (1){
+    mnu:
+      scrClear(tcO,FALSE);  
+      strOut(tcO,HEAD0);
+      strOut(tcO,HEAD1);
+      strOut(tcO, L"\r\n");
+      strOut(tcO, L"BD_PROCHOT: ");
+      strOut(tcO, (AsmReadMsr64(0x1FC) & 1) ? L"1" : L"0" ) ;
+
+      menu(choice);
+
+    sT->BootServices->WaitForEvent(1, &sT->ConIn->WaitForKey, &index);
+    sT->ConIn->ReadKeyStroke(sT->ConIn, &key);
+    keychar=key.UnicodeChar;
+    if (keychar >= '0' && keychar <= '3') {
+      choice = keychar - '0';
+      break;
+    }else if(key.ScanCode == SCAN_UP){
+      choice=(choice+3)%4;
+      goto mnu;
+    }else if(key.ScanCode == SCAN_DOWN){
+      choice=(choice+1)%4;
+      goto mnu;
+    }else if (keychar == CHAR_CARRIAGE_RETURN ) {
+      break;
+    }else goto mnu;  
+  }
+  return choice; 
+}
+
+void
+table(uint64_t term,CHAR16 *op, uint64_t result){
+  CHAR16 *tblHL   = TBL_HL ;
+  strRow(op,term); 
+  strOut(tcO,L"\r\n");
+  strOut(tcO,tblHL);
+  strRow(L"RESULT: ",result);
+}
+
+void
+strRow(CHAR16 *head,uint64_t data){
+  strOut(tcO, L"\r\n");
+  strOut(tcO, head);
+  for (int i = 63; i >= 0; i--) {
+    strOut(tcO, (data & (1ULL << i)) ? L"1" : L"0");
+  }
+}
+
+void
+set_ProcHot() {
+  uint64_t val = read_MSR();
+  uint64_t setbitzero = SET_ZERO;  
+  uint64_t nval = val | setbitzero;
+  table(setbitzero,L"    OR: ",nval);
+  ConfirmAndWrite(nval,MSG_ENABLED);
+}
+
+void
+clear_ProcHot(){
+  uint64_t val = read_MSR();
+  uint64_t clearbitzero =  CLEAR_ZERO;
+  uint64_t nval = val & clearbitzero;    
+  table(clearbitzero,L"   AND: ",nval);
+  ConfirmAndWrite( nval, MSG_DISABLED); 
+}
+
+uint64_t
+read_MSR(){
+  uint64_t val = AsmReadMsr64(MSR_ADDRESS);
+  CHAR16 *tblADR  =L"\r\n 0x1FC: ";
+  strRow(tblADR,val);
+  return val;  
+}
+
--- build.c.sh
+++ build.c.sh
@@ -1,13 +1,69 @@
 #!/usr/bin/env bash
-gcc -I$PWD/headers -I/usr/include/efi -I/usr/include/efi/x86_64 -I/home/hoefkens/Development/Git/Github/ZeroProcHot_3/efi -DGNU_EFI_USE_MS_ABI -Dx86_64 -fPIC -fshort-wchar -ffreestanding -fno-stack-protector -maccumulate-outgoing-args -Wall -m64 -mno-red-zone -c -oZeroProcHot.c.o ZeroProcHot.c 
+NAME="ZeroProcHot"
+VER="0v401"
+REFINDDRIVERS="$1/EFI/reFind/drivers_x64"
 
+
+LIBGCC="/usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/libgcc.a"
+LIBGNUEFI="/usr/lib64/libgnuefi.a"
+EFIHEADERS="$PWD/headers"
+EFIINCLUDE="/usr/include/efi"
+EFIINCLUDEX64="/usr/include/efi/x86_64"
+ELFX64LDS="/usr/lib64/elf_x86_64_efi.lds"
+EFICRT="/usr/lib64/crt0-efi-x86_64.o"
+TARGET="efi-app-x86_64"
+
+
+
+
+mkdir -p "$PWD/build/bin"
+
+
+GCCPARAMS=(
+    -I${EFIHEADERS}
+    -I${EFIINCLUDE}
+    -I${EFIINCLUDEX64}
+    -DGNU_EFI_USE_MS_ABI
+    -Dx86_64
+	-fPIC
+	-fshort-wchar 
+	-ffreestanding 
+	-fno-stack-protector
+	-maccumulate-outgoing-args 
+	-Wall
+	-m64 
+	-mno-red-zone 
+	-c
+)
+LDPARAMS=(
+	-T ${ELFX64LDS}
+	-Bsymbolic 
+	-shared 
+	-nostdlib 
+	-znocombreloc ${EFICRT}
+)
+
+OBJCPYPARAMS=(
+	-j .text 
+	-j .sdata 
+	-j .data 
+	-j .dynamic 
+	-j .dynsym 
+	-j .rel 
+	-j .rela 
+	-j .reloc 
+	-S 
+	--target=$TARGET	
+)
+
+gcc  "${GCCPARAMS[@]}" "-obuild/${NAME}.c.$VER.o" "${NAME}.c" 
  
-ld -T /usr/lib64/elf_x86_64_efi.lds -Bsymbolic -shared -nostdlib -znocombreloc /usr/lib64/crt0-efi-x86_64.o -o ZeroProcHot.c.so ZeroProcHot.c.o /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/libgcc.a /usr/lib64/libgnuefi.a
+ld "${LDPARAMS[@]}" "-obuild/${NAME}.c.$VER.so" "build/${NAME}.c.$VER.o" "${LIBGCC}" "${LIBGNUEFI}"
 
-objcopy -j .text -j .sdata -j .data -j .dynamic -j .dynsym -j .rel -j .rela -j .reloc -S --target=efi-app-x86_64 ZeroProcHot.c.so ZeroProcHot_x64.efi
+objcopy  "${OBJCPYPARAMS[@]}" "build/${NAME}.c.$VER.so" "build/bin/${NAME}_${VER}_x64.efi"
 
-ls -l ZeroProcHot_x64.efi
-md5sum ZeroProcHot_x64.efi
-sudo cp -v ZeroProcHot_x64.efi "$1"
-md5sum "${1}/ZeroProcHot_x64.efi"
-sudo cp -v "${1}/ZeroProcHot_x64.efi" "${1}/bootx64.efi"
+ls -l "build/bin/${NAME}_${VER}_x64.efi"
+md5sum "build/bin/${NAME}_${VER}_x64.efi"
+sudo cp -v "build/bin/${NAME}_${VER}_x64.efi" "${REFINDDRIVERS}/${NAME}_x64.efi"
+
+
